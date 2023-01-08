2f19871fc539520002fabe52ab45cdd2720beb25
nokpil <58059577+nokpil@users.noreply.github.com>
2023-01-08 14:50:26 +0900

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -42,8 +42,6 @@ import random
 from collections import OrderedDict
 from copy import deepcopy
 
-import src.DataStructure as DS
-
 ## Data type conversion
 
 def DCN(x):
@@ -784,30 +782,6 @@ def cfg_Block(block_type, cfg, D_agent, nl_type='RL', batch_norm=False, drop_out
     else:
         print("NOT IMPLEMENTED : cfg_Block")
     return block
-
-class AverageMeter(object):
-    """Computes and stores the average and current value"""
-
-    def __init__(self, name, fmt=":f"):
-        self.name = name
-        self.fmt = fmt
-        self.reset()
-
-    def reset(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
-
-    def update(self, val, n=1):
-        self.val = val
-        self.sum += val * n
-        self.count += n
-        self.avg = self.sum / self.count
-
-    def __str__(self):
-        fmtstr = "{name} {val" + self.fmt + "} ({avg" + self.fmt + "})"
-        return fmtstr.format(**self.__dict__)
 
 
 if __name__ == "__main__":
